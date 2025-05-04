2e73c300da91de5c92f7ec040e2eaa3f15ff6ffe
vladmelnykk <vlad.melnyk28@gmail.com>
2025-05-04 16:50:28 +0300

    add redux-persist for state persistence

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
 import './index.css';
 import TicTacToeProvider from './providers/TicTacToeProvider.tsx';
-import { store } from './store/store.ts';
-
+import { persistor, store } from './store/store.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <TicTacToeProvider>
-          <App />
-        </TicTacToeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <TicTacToeProvider>
+            <App />
+          </TicTacToeProvider>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/userSlice';
 
-export const store = configureStore({
-  reducer: { user: userReducer }
+const rootReducer = combineReducers({
+  user: userReducer
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
